abdbbe05aa644aada9cf8ed498abb64d69270b8a
Gustavo Souza <gustavogss.dev@gmail.com>
2023-02-23 09:21:53 -0300

    Fazendo tela Home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,17 @@
-import {View, Text} from 'react-native';
+import {View, Image, Text} from 'react-native';
 import React from 'react';
 import styles from './styles';
+import ligthOn from '../assets/images/ecolight-on.png';
+import ligthOf from '../assets/images/ecolight-off.png';
 
 export default function Home() {
   const toggle = true;
   return (
     <View style={toggle ? styles.container : styles.containerLigth}>
-      <Text>Home</Text>
+      <Image source={toggle ? ligthOf : ligthOn} style={styles.ligth} />
+      <Text style={toggle ? styles.textLogo : styles.textLogoLigth}>
+        PlayLigth
+      </Text>
     </View>
   );
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -3,15 +3,35 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#212121',
     alignItems: 'center',
     justifyContent: 'center',
   },
   containerLigth: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  ligth: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+  textLogo: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  textLogoLigth: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 20,
+    fontWeight: 'bold',
+    color: '#032e52',
   },
 });
 
